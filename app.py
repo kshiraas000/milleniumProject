3c94844d8034e0fb2cb603e661f9df068967a3d5
sarabandi3 <sarabandi3@gatech.edu>
2025-03-25 14:15:22 -0400

    Frontend to Backend Connection

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from datetime import datetime
 from random import choice
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 load_dotenv()  # Load environment variables from .env file
 # we need to input our MYSQL database here
